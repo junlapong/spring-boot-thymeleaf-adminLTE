9624d94e464e939f05d94be59b9808ab5a528faa
Junlapong L <junlapong@gmail.com>
2016-11-12 03:50:08 +0700

    initial

---
+++ src/main/java/pl/codeleak/demos/sbt/Application.java
@@ -0,0 +1,34 @@
+package pl.codeleak.demos.sbt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
+
+    public static void main(String[] args) {
+        logger.info("Starting Server!");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        logger.info("Starting Servlet");
+        return application.sources(Application.class);
+    }
+
+}
---
+++ src/main/java/pl/codeleak/demos/sbt/home/HomeController.java
@@ -0,0 +1,31 @@
+package pl.codeleak.demos.sbt.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
+import java.time.LocalDateTime;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+        return "index";
+    }
+
+    @RequestMapping("/demo")
+    public RedirectView localRedirect() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("pages/widgets.html");
+        return redirectView;
+    }
+
+    // @RequestMapping("properties")
+    // @ResponseBody
+    // java.util.Properties properties() {
+    //     return System.getProperties();
+    // }
+}
